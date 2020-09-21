dd845a6952651c9d5aad12db39541e4ed0f0a3f1
scottwcode <scottmail77@gmail.com>
2020-09-21 13:47:00 -0400

    added biz columns and sample data (hardcoded) to table - no sorting or searching yet

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Biz Buyer Portal`,
+    description: `A Portal where you can view, sort and filter businesses that are for sale from several publicly listed websites.`,
     author: `@gatsbyjs`,
   },
   plugins: [
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,65 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
+
+import { Container, Table } from "react-bootstrap"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Container>
+      <h1>Businesses for Sale</h1>
+      <Table responsive="md" striped bordered hover className="noWrap">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Asking_Price</th>
+            <th>Cash_Flow</th>
+            <th>PE</th>
+            <th>City</th>
+            <th>State</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              Health and Personal Care Amazon FBA eCommerce Retailer - Solid YOY
+              Growth
+            </td>
+            <td>47000000</td>
+            <td>8779435</td>
+            <td>5.35</td>
+            <th>Richmond</th>
+            <th>VA</th>
+          </tr>
+          <tr>
+            <td>
+              Social Media Driven eCommerce Retailer - Women's Fashion - All
+              Sales on Branded Website - Massive Growth
+            </td>
+            <td>30000000</td>
+            <td>870005</td>
+            <td>4.22</td>
+            <th>Richmond</th>
+            <th>VA</th>
+          </tr>
+          <tr>
+            <td>
+              Branded Amazon FBA Office Supplies and Kitchen Goods eCommerce
+              Retailer
+            </td>
+            <td>3450000</td>
+            <td>770785</td>
+            <td>3.26</td>
+            <th>Richmond</th>
+            <th>VA</th>
+          </tr>
+        </tbody>
+      </Table>
+    </Container>
   </Layout>
 )
 
