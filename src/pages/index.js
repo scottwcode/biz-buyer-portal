77edbf0274e1772992de43086e94c034c9b92c0c
scottwcode <scottmail77@gmail.com>
2020-09-30 09:59:04 -0400

    Added datatable code. Currently retrieves from a Contacts API endpoint, need to change to firestore biz listings or JSON

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,46 @@ import Table from "react-bootstrap/Table"
 import Button from "react-bootstrap/Button"
 // import { Container, Table, Button } from "react-bootstrap"
 
+// // new code \/
+// import ReactDOM from "react-dom"
+// import App from "../app"
+
+// const IndexPage = () => (
+//   <Layout>
+//     <SEO title="Home" />
+//     <Container>
+//       <h1>Businesses for Sale</h1>
+//       {/* // ReactDOM.render( */}
+//       {/* <React.StrictMode> */}
+//       <App />
+//       {/* </React.StrictMode> */}, document.getElementById("root")
+//       {/* // ) */}
+//     </Container>
+//     <br />
+//     <br />
+//     <Link to="/listing-table/">GraphQL Query - JSON - Biz Listings</Link>
+//     <br />
+//     <br />
+//     <Link to="/graphql-firestore-biz-listings/">
+//       GraphQL Query - Firestore - Biz Listings
+//     </Link>
+//     <br />
+//     <br />
+//     <Link to="/json-biz-data-table1/">JSON Data - Table1 page</Link>
+//     <br />
+//     <br />
+//     <Link to="/json-biz-data-table2/">JSON Data - Table2 page</Link>
+//     <br />
+//     <br />
+//     <Link to="/graphql-firestore-biz-datatable/">
+//       GraphQL Query - Firestore - Datatable
+//     </Link>
+//     <br />
+//   </Layout>
+// )
+// // new code ^
+
+// // Commented out all of the below for testing datatable
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
